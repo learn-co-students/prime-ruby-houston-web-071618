c13ae86e3d174271db5306e655fd6121f29e2f92
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-06-27 13:26:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+
+def prime?(integer)
+  range = (2...integer)
+  if integer<=1
+    return FALSE
+  else
+    range.each do |x| 
+      if integer%x==0 
+        return FALSE
+      end
+    end
+    return TRUE
+  end
+  
+end
