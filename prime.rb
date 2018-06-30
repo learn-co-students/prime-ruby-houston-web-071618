685364a707d9094f6dc7a4d41a5204a9fc565539
Scott Tang <scott.q.tang@gmail.com>
2018-06-30 13:06:24 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+def prime?(n)
+    
+    if n == 0 || n == 1 || n == -1
+        is_prime = false
+    else
+        is_prime = true
+    end
+
+    for i in 2..n-1
+        if n % i == 0
+            is_prime = false
+        end
+    end
+    
+    if is_prime
+        true
+    else
+        false
+    end
+end
+
+puts prime?(1)
+
